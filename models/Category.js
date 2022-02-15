ef5d950004c107c24655610a66f0346482687f17
Coder J <buildingguy@sbcglobal.net>
2022-02-15 15:49:43 -0800

    set model for categories

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,41 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    appliances: {
+      type: DataTypes.BOOLEAN
+    },
+    appsGames: {
+      type: DataTypes.BOOLEAN
+    },
+    clothingShoesJewerly: {
+      type: DataTypes.BOOLEAN
+    },
+    beauty: {
+      type: DataTypes.BOOLEAN
+    },
+    electronics: {
+      type: DataTypes.BOOLEAN
+    },
+    outdoor: {
+      type: DataTypes.BOOLEAN
+    },
+    grocery: {
+      type: DataTypes.BOOLEAN
+    },
+    entertainment: {
+      type: DataTypes.BOOLEAN
+    },
+    petSupplies: {
+      type: DataTypes.BOOLEAN
+    },
+    toys: {
+      type: DataTypes.BOOLEAN
+    },
   },
   {
     sequelize,
