db072089a4613018f43119fac70704976355905c
Coder J <buildingguy@sbcglobal.net>
2022-02-15 15:30:37 -0800

    set up product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    brand: {
+      type: DataTypes.STRING
+    },
+    manufacture: {
+      type: DataTypes.STRING
+    },
+    isbn: {
+      type: DataTypes.STRING
+    },
+    price: { 
+      type: DataTypes.DECIMAL
+    },
+    SKU: { //bar code (scan), individual type
+      type: DataTypes.STRING
+    },
+    weight: {
+      type: DataTypes.DECIMAL
+    }
   },
   {
     sequelize,
